c7d5c392e88cd1942406a2007616381e225bfbbe
mahirsaleh <129379291+mahirsaleh@users.noreply.github.com>
2025-06-29 23:56:33 +0600

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,34 @@
+"use strict" ;
+
+const p = document.querySelector('.khelaHobe') ;
+const innerP = p.innerText ;
+let clearWatchId ;
+
+const historyThunder = () => {
+    if (p.innerText === innerP) {
+        if (window.navigator.geolocation) {
+
+            clearWatchId = window.navigator.geolocation.watchPosition(
+                (object) => {
+                    const location = `Latitude : ${object.coords.latitude}
+                    Longitutde : ${object.coords.longitude}` ;
+                    
+                    p.innerText = location ;
+                    console.log(clearWatchId, 'open') ;
+                }, 
+                (error) => {
+                    p.innerText = error.message.replaceAll(/user/ig, 'You') ;
+                    console.log(clearWatchId, 'error') ;
+                },    
+            ) ;
+        } 
+        else {
+            window.alert('Nothing Bro !') ;
+        } ;
+    }
+    else {
+        console.log(clearWatchId, 'clear')
+        window.navigator.geolocation.clearWatch(clearWatchId) ;
+        p.innerText = innerP ;
+    }
+} ;
